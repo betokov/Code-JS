cbd30091b0a8243cba303e4a53b56420d099cf63
betokov <web_masters_07@mail.ru>
2019-03-31 16:50:36 +0300

    last ubuntu

---
+++ autocomplete/main.js
@@ -0,0 +1,80 @@
+ /*
+    ****Autocomplete***
+ */
+
+ var list = document.getElementById("list"),
+ listDefault =  ['Африка', 'Бразилия', 'Великобритания', 'Велинктон', 'Греция', 'Дания'],
+ div = document.createElement("div"),
+ span = document.createElement("span");
+
+
+//Search elements in array
+ var search = function() {
+
+ 	container.style.display = "none";
+ 	container.innerHTML = "";
+
+ 	var newArr = [];
+
+ 	for (var i = 0; i < listDefault.length; i++) {
+
+ 		newArr.push(listDefault[i].toLowerCase());
+
+ 		if(newArr[i].indexOf(list.value.toLowerCase()) !== -1){
+ 			container.style.display = "block";
+ 			var span = document.createElement("span");
+ 			container.appendChild(span).innerText = listDefault[i];
+
+ 		}
+
+ 	}
+ }
+
+
+//Focus input
+ var infocus = function (){
+
+ 	document.body.appendChild(div).setAttribute("id", "container");
+
+ 	if(list.value == ''){
+
+ 		for (var i = 0; i < listDefault.length; i++) {
+
+ 			var span = document.createElement("span");
+ 			container.appendChild(span).innerText = listDefault[i];
+
+ 		}
+
+ 		container.style.display = "block";
+
+ 	} else {
+
+ 		search();
+
+ 	}
+
+ }
+
+
+//Unfocus input
+ var inblur = function(){
+ 	container.innerHTML = "";
+ 	container.style.display = "none";
+ }
+
+//KeyUp
+ var inkeyup = function (){
+ 	search();
+ }
+
+
+
+
+ list.addEventListener("focus", infocus);
+ list.addEventListener("blur", inblur);
+ list.addEventListener("keyup", inkeyup);
+
+
+
+
+
